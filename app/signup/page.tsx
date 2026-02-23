ad6a3255adc4445aae6d7e2aa5df67003d9d1828
T-Rex_Coder <sanjaylade92220@gmail.com>
2026-02-23 22:27:03 +0530

    imported req lib

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,3 +1,11 @@
+"use client";
+import React from "react";
+import Link from "next/link";
+import {useRouter} from "next/navigation";
+import { Axios } from "axios";
+
+
+
 export default function SignupPage() {
     return (
         <div className="flex">  
