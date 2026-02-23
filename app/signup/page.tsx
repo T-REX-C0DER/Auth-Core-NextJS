072c088384b65eb24bf6e027c81181b8a67219e3
T-Rex_Coder <sanjaylade92220@gmail.com>
2026-02-23 22:29:46 +0530

    created signup func

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -7,6 +7,19 @@ import { Axios } from "axios";
 
 
 export default function SignupPage() {
+    const [user,setuser] = React.useState({
+
+        email:"",
+        password:"",
+        username:"",
+
+    })
+
+    const onSignup = async () => {
+
+    }
+
+    
     return (
         <div className="flex">  
          <h1 className="text-5xl font-bold text-white "> SIGNUP PAGE</h1>
